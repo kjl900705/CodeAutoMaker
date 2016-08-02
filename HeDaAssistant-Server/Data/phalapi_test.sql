b0e7513ed88c35d17886a82c0d679b43d47836f5
xiongcaichang <15223245@qq.com>
2016-08-02 12:21:06 +0800

    Push  init

--- HeDaAssistant-Server/Data/phalapi_test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `demo`.`tbl_user` (
-      `id` INT NOT NULL,
-      `name` VARCHAR(45) NULL,
-      `note` VARCHAR(45) NULL,
-      PRIMARY KEY (`id`));
-
-
-INSERT INTO `demo`.`tbl_user` (`id`, `name`, `note`) VALUES ('1', 'xcc', 'oschina');
-
